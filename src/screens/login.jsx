ec81f3b4ee0945ad3116760a4553c0298cd9dd1d
SebastianLeivaaa <sebastianleiva010@gmail.com>
2024-05-12 19:05:33 -0400

    Corrigiendo detalles para que sea responsive el login

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -7,19 +7,20 @@ import { PasswordInput } from "../components/passwordInput";
 export const Login = () => {
 
     return (
-        <div className="h-full w-full flex flex-col items-center justify-center gap-y-8 mt-8">
+        <div className="w-full flex flex-col items-center justify-center gap-y-8 py-8">
             <form className="flex flex-col items-center gap-y-8 w-full">
                 <div className="w-full flex flex-row">
-                    <input type="text" className="w-[90%] p-2 border-[0.5px] border-blue-ribbon-600 max-md:w-[85%] " placeholder="Correo Electrónico"/>
-                    <div className="w-[10%] border-[0.5px] border-l-[0px] border-blue-ribbon-600 flex items-center justify-center max-md:w-[15%]">
+                    <div className="w-[10%] border-[0.5px] border-r-[0px] border-blue-ribbon-600 flex items-center justify-center max-md:w-[15%]">
                         <MdEmail className="text-blue-ribbon-600 text-2xl"/>
                     </div>
+                    <input type="text" className="w-[90%] p-2 border-[0.5px] border-blue-ribbon-600 max-md:w-[85%] " placeholder="Correo Electrónico"/>
                 </div>
                 <div className="w-full flex flex-row">
-                    <PasswordInput/>
-                    <div className="w-[10%] border-[0.5px] border-l-[0px] border-blue-ribbon-600 flex items-center justify-center max-md:w-[15%]">
+                    <div className="w-[10%] border-[0.5px] border-r-[0px] border-blue-ribbon-600 flex items-center justify-center max-md:w-[15%]">
                         <MdLock className="text-blue-ribbon-600 text-2xl"/>
                     </div>
+                    <PasswordInput/>
+                   
                 </div>
                 <button className="mt-8 w-full bg-blue-ribbon-600 font-bold text-white px-4 py-2 rounded-md text-lg hover:bg-blue-ribbon-700 flex flex-row items-center justify-center gap-x-2 max-md:text-base"><MdLogin className="text-3xl max-md:text-2xl"/> INGRESAR</button>
             </form>
--- src/screens/signIn.jsx
+++ src/screens/signIn.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Indice } from "../components/indice";
 
 export const SignIn = () => {
     return (
         <div className="h-full w-full flex items-center justify-center flex-col">
+            <Indice/>
             <form className="flex flex-col items-center gap-y-4 w-full">
                 <h1>HOLAA</h1>
             </form>
