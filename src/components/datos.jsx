395a24c3bf159665034c8327e7878218f4d9d20c
Thomas Pinda <90642090+thomaspinda@users.noreply.com>
2024-05-12 21:13:20 -0400

    singIn

---
+++ src/components/datos.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export const Datos = (props) => {
+  return(
+    <div className="w-full flex flex-col ml-4" >
+      <h1 className="pb-3">{props.titulo}</h1>
+      <input type="text" className="w-[90%] p-1 border-[0.5px] border-blue-ribbon-600 max-md:w-[55%] " placeholder={props.holder}/>
+    </div>
+  );
+  
+}
+
+export default Datos
