3360fde54570caca855dd8095ee72f6b0dde1738
SebastianLeivaaa <sebastianleiva010@gmail.com>
2024-05-13 22:48:08 -0400

    Interfaz registro 2, validaciones para la clave y implementacion de envios de correos electronicos

---
+++ src/utils/mailer.js
@@ -0,0 +1,42 @@
+import { google } from 'googleapis';
+import nodemailer from 'nodemailer';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
+
+export const sendCodeEmail = async (code, to) => {
+  const ACCESS_TOKEN = await oAuth2Client.getAccessToken();
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.MY_EMAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: ACCESS_TOKEN,
+    },
+    tls: {
+      rejectUnauthorized: true,
+    },
+  });
+  const subject = 'Código de verificación'
+  
+  const html = `
+    <p>Codigo de verificacion: ${code}</p>
+    `;
+  return new Promise((resolve, reject) => {
+    transport.sendMail({ from: process.env.MY_EMAIL, subject: subject, to: to, html }, (err, info) => {
+      if (err) reject(err);
+      resolve(info);
+    });
+  });
+};
