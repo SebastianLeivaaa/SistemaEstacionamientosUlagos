0523ab2058ca80c4b3b1e2b84c67be1526bce537
Thomas Pinda <90642090+thomaspinda@users.noreply.com>
2024-05-25 12:01:32 -0400

    Responsive User

--- src/screens/user/usermenu.jsx
+++ src/screens/user/usermenu.jsx
@@ -21,7 +21,7 @@ export const Usermenu = () => {
 
     const getProfile = async () => {
         try{
-            const response = await axios.get("/api/login", {withCredentials: true});
+            const response = await axios.get("http://localhost:3090/api/login", {withCredentials: true});
             setUser({
                 email: response.data.email,
                 username: response.data.username,
@@ -69,7 +69,7 @@ export const Usermenu = () => {
     return (
         <div className="min-h-screen w-screen flex items-center justify-center bg">
             <div className="flex flex-col items-center p-8 gap-y-8 rounded-md max-md:w-[75%] max-md:px-4 max-md:py-8 bg-white-50">
-                <div className="flex flex-wrap sm:flex-row w-full justify-between">
+                <div className="flex flex-wrap lg:flex-row w-full justify-end sm:justify-between lg:justify-between">
                     <img src={Ulogo} alt="Logo Ulagos" className="px-5 w-full sm:w-60 h-auto"/>
                     <div className="flex flex-col items-end justify-end mt-4">
                         <h1 className="text-lg font-bold text-center max-md:text-base text-congress-blue-900">{user.username.toLocaleUpperCase()} {user.userLastNamePat.toLocaleUpperCase()} {user.userLastNameMat.toLocaleUpperCase()}</h1>
@@ -87,13 +87,13 @@ export const Usermenu = () => {
                         ¡{parkingSpaces !== null ? parkingSpaces : 'Cargando...'} ESTACIONAMIENTOS DISPONIBLES!
                     </h1>
                 </div>
-                <div className="flex flex-col sm:flex-row justify-between w-[90%] gap-y-4">
-                    <button className="w-full sm:w-[42%] bg-blue-ribbon-600 text-white-100 font-bold rounded-md p-4 text-sm sm:text-lg flex flex-row items-center justify-center gap-x-2"><FaCarAlt className="text-3xl"/>RESERVAR ESTACIONAMIENTO</button>
-                    <button onClick={reservations} className="w-full sm:w-[42%] bg-blue-ribbon-600 text-white-100 font-bold rounded-md p-4 text-sm sm:text-lg flex flex-row items-center justify-center gap-x-2"><LuHistory className="text-3xl"/>MIS RESERVAS</button>                
+                <div className="flex flex-col sm:flex-row justify-between w-[90%] gap-y-5">
+                    <button className="w-full sm:w-[48%] bg-blue-ribbon-600 text-white-100 font-bold rounded-md p-4 text-sm sm:text-sm lg:text-lg flex flex-row items-center justify-center gap-x-2"><FaCarAlt className="text-3xl"/>RESERVAR ESTACIONAMIENTO</button>
+                    <button onClick={reservations} className="w-full sm:w-[48%] bg-blue-ribbon-600 text-white-100 font-bold rounded-md p-4 text-sm sm:text-sm lg:text-lg flex flex-row items-center justify-center gap-x-2"><LuHistory className="text-3xl"/>MIS RESERVAS</button>                
                 </div>
                 <div className="flex flex-col sm:flex-row justify-between w-[90%] gap-y-4">
-                    <button  className="w-full sm:w-[42%] bg-blue-ribbon-600 text-white-100 font-bold rounded-md p-4 text-sm sm:text-lg flex flex-row items-center justify-center gap-x-2"><FaMapMarked className="text-3xl"/>MAPA DE ESTACIONAMIENTO</button>
-                    <button onClick={manageVehicle} className="w-full sm:w-[42%] bg-blue-ribbon-600 text-white-100 font-bold rounded-md p-4 text-sm sm:text-lg flex flex-row items-center justify-center gap-x-2"><FaCarAlt className="text-3xl"/>ACTUALIZAR DATOS DE VEHÍCULO</button>                
+                    <button  className="w-full sm:w-[48%] bg-blue-ribbon-600 text-white-100 font-bold rounded-md p-4 text-sm sm:text-sm lg:text-lg flex flex-row items-center justify-center gap-x-2"><FaMapMarked className="text-3xl"/>MAPA DE ESTACIONAMIENTO</button>
+                    <button onClick={manageVehicle} className="w-full sm:w-[48%] bg-blue-ribbon-600 text-white-100 font-bold rounded-md p-4 text-sm sm:text-sm lg:text-lg flex flex-row items-center justify-center gap-x-2"><FaCarAlt className="text-3xl"/>ACTUALIZAR DATOS DE VEHÍCULO</button>                
                 </div>
             </div>
         </div>
