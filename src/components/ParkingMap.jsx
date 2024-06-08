b1744d5498e3c1bb54f9fff161c6c00a02049e0f
SebastianLeivaaa <sebastianleiva010@gmail.com>
2024-06-07 23:11:10 -0400

    Mapa corregido

--- src/components/ParkingMap.jsx
+++ src/components/ParkingMap.jsx
@@ -1,37 +1,37 @@
 import React, { useEffect, useState, useRef } from 'react';
 import Mapa from "../assets/img/Map.png";
 import imageMapResize from "../utils/mageMapResizer.min";
-
-const Modal = ({ area, onClose }) => {
-  return (
-    <div className=" fixed inset-0 flex items-center justify-center">
-      <div className=" bg-white-200 p-4 rounded shadow-lg">
-        <h2 className="text-lg font-bold mb-2">{area.name}</h2>
-        <p>Información sobre {area.name}</p>
-        <button onClick={onClose} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">Cerrar</button>
-      </div>
-    </div>
-  );
-};
+import axios from "axios";
 
 export const ParkingMap = () => {
   const [hoveredArea, setHoveredArea] = useState(null);
-  const [scaledAreas, setScaledAreas] = useState([{ name: 'Zona A', scaledCoords: [401,386,407,432,550,416,667,433,666,382,577,369,516,369], shape: 'poly' },
-  { name: 'Zona B', scaledCoords: [303,133,332,390,400,384,398,331,375,332,360,201,375,199,370,137,348,137], shape: 'poly' },
-  { name: 'Zona C', scaledCoords: [103,428,336,408,340,450,114,476], shape: 'poly' },
-  { name: 'Zona D', scaledCoords: [37,365,99,364,103,429,55,434,37,417], shape: 'poly' },
-  { name: 'Zona E', scaledCoords: [43,573,82,571,98,641,106,642,107,687,131,685,133,709,143,711,146,760,61,764], shape: 'poly' }]);
-  const [selectedArea, setSelectedArea] = useState(null);
+  const [scaledAreas, setScaledAreas] = useState([]);
+  const [areas, setAreas] = useState([
+    { name: 'SECCIÓN A', coords: '401,386,407,432,550,416,667,433,666,382,577,369,516,369', shape: 'poly', parkigsAvailables: null },
+    { name: 'SECCIÓN B', coords: '303,133,332,390,400,384,398,331,375,332,360,201,375,199,370,137,348,137', shape: 'poly', parkigsAvailables: null },
+    { name: 'SECCIÓN C', coords: '103,428,336,408,340,450,114,476', shape: 'poly', parkigsAvailables: null },
+    { name: 'SECCIÓN D', coords: '37,365,99,364,103,429,55,434,37,417', shape: 'poly', parkigsAvailables: null },
+    { name: 'SECCIÓN E', coords: '43,573,82,571,98,641,106,642,107,687,131,685,133,709,143,711,146,760,61,764', shape: 'poly', parkigsAvailables: null },
+  ]);
   const mapRef = useRef(null);
   const imgRef = useRef(null);
 
-  const areas = [
-    { name: 'Zona A', coords: '401,386,407,432,550,416,667,433,666,382,577,369,516,369', shape: 'poly' },
-    { name: 'Zona B', coords: '303,133,332,390,400,384,398,331,375,332,360,201,375,199,370,137,348,137', shape: 'poly' },
-    { name: 'Zona C', coords: '103,428,336,408,340,450,114,476', shape: 'poly' },
-    { name: 'Zona D', coords: '37,365,99,364,103,429,55,434,37,417', shape: 'poly' },
-    { name: 'Zona E', coords: '43,573,82,571,98,641,106,642,107,687,131,685,133,709,143,711,146,760,61,764', shape: 'poly' },
-  ];
+  const getParkingsAvailablesBySection = async () => {
+    try {
+      const response = await axios.get("/api/get-parkings-availables-by-section", { withCredentials: true });
+      const data = response.data;
+
+      // Actualizar las áreas con la cantidad de estacionamientos disponibles
+      const updatedAreas = areas.map(area => {
+        const matchedSection = data.find(section => section.secc_nombre === area.name);
+        return matchedSection ? { ...area, parkigsAvailables: matchedSection.cantidad_estacionamientos } : area;
+      });
+
+      setAreas(updatedAreas);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleMouseEnter = (area) => {
     setHoveredArea(area);
@@ -41,34 +41,25 @@ export const ParkingMap = () => {
     setHoveredArea(null);
   };
 
-  const handleClick = (area) => {
-    setSelectedArea(area);
-  };
-
-  const closeModal = () => {
-    setSelectedArea(null);
-  };
-
   const parseCoords = (coords) => {
     return coords.split(',').map(Number);
   };
 
   useEffect(() => {
-    console.log(scaledAreas)
+    getParkingsAvailablesBySection();
     imageMapResize();
-    updateScaledAreas();
   }, []);
-  
+
   useEffect(() => {
     window.addEventListener('resize', updateScaledAreas);
     return () => {
       window.removeEventListener('resize', updateScaledAreas);
     };
   }, []);
-  
+
   useEffect(() => {
     updateScaledAreas();
-  }, [imgRef.current, mapRef.current]);
+  }, [imgRef.current, mapRef.current, areas]);
 
   const updateScaledAreas = () => {
     if (!mapRef.current || !imgRef.current) return;
@@ -109,7 +100,7 @@ export const ParkingMap = () => {
               alt={area.name}
               onMouseEnter={() => handleMouseEnter(area.name)}
               onMouseLeave={handleMouseLeave}
-              className='outline-none cursor-pointer'
+              className='cursor-pointer outline-none'
               tabIndex="0"
             />
           );
@@ -120,14 +111,11 @@ export const ParkingMap = () => {
           <polygon
             key={index}
             points={area.scaledCoords.join(',')}
+            id={area.parkigsAvailables}
             className={`fill-white-50 hover:fill-blue-500 ${hoveredArea === area.name ? 'opacity-75' : 'opacity-50'}`}
-            onMouseEnter={() => handleMouseEnter(area.name)}
-            onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick(area)}
           />
         ))}
       </svg>
-      {selectedArea && <Modal area={selectedArea} onClose={closeModal} />}
     </div>
   );
 };
