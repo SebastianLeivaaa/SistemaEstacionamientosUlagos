d205f316fd12e559624d2edae646ff8a5a030cec
SebastianLeivaaa <sebastianleiva010@gmail.com>
2024-06-10 23:02:00 -0400

    Cambiando select de usario registro

--- src/components/selectUser.jsx
+++ src/components/selectUser.jsx
@@ -5,7 +5,7 @@ export const SelectUser = (props) => {
   return(
     <select id={props.id} name={props.name} className="w-[90%] p-1 border-[0.5px] border-blue-ribbon-600" placeholder={props.holder} onChange={props.onChange} value={props.value}>
       <option>Estudiante</option>
-      <option>Funcionario</option>
+      <option>Profesor</option>
       <option>Externo</option>
     </select>
   );
