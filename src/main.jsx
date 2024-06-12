923588c3e8c38891713cee8e9e6a6286a1644a7a
SebaLeivaa <sebastiannicolas.leiva@alumnos.ulagos.cl>
2024-06-12 17:26:55 -0400

    Probando analytics de vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './tailwind/index.css';
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Analytics/>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
