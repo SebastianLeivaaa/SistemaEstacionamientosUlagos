ae4e72d273436cab0d3e93247aa504a392061936
SebaLeivaa <sebastiannicolas.leiva@alumnos.ulagos.cl>
2024-06-24 00:33:31 -0400

    Corrigiendo cookies

--- api/index.js
+++ api/index.js
@@ -938,22 +938,26 @@ app.get('/api/login', async (req, res) => {
 });
 
 //logout
-app.get('/api/logout', async (req, res) => {
+app.get('/api/logout', (req, res) => {
   const myTokenName = req.cookies.myTokenName;
+
   if (!myTokenName) {
     return res.status(401).json({ error: 'no token' });
   }
+
+  const serialized = serialize('myTokenName', '', {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: -1,
+    path: '/'
+  });
+
+  res.setHeader('Set-Cookie', serialized);
+
   try {
     jwt.verify(myTokenName, process.env.SECRET);
-    const serialized = serialize('myTokenName', '', {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Solo true en producción
-      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax', // Cambiar a 'lax' en desarrollo
-      maxAge: 0,
-      path: '/'
-    });
-    res.setHeader('Set-Cookie', serialized);
-    res.status(200).json('logout successfully');
+    return res.status(200).json('logout successfully');
   } catch (error) {
     return res.status(401).json({ error: 'invalid token' });
   }
