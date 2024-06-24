542908f509c72f2aea90cf99cf4ac4a3c56764a9
SebaLeivaa <sebastiannicolas.leiva@alumnos.ulagos.cl>
2024-06-24 02:55:03 -0400

    adsda

--- api/index.js
+++ api/index.js
@@ -946,14 +946,7 @@ app.get('/api/logout', async (req, res) => {
   try {
     jwt.verify(myTokenName, process.env.SECRET);
     jwt.destroy(myTokenName);
-    const serialized = serialize('myTokenName', '', {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Solo true en producción
-      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax', // Cambiar a 'lax' en desarrollo
-      maxAge: 0,
-      path: '/'
-    });
-    res.setHeader('Set-Cookie', serialized);
+
     res.status(200).json('logout successfully');
   } catch (error) {
     return res.status(401).json({ error: 'invalid token' });
